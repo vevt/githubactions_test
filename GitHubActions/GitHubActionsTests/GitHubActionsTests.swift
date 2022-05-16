3d5d6995e8432d974d93cb4f23275cc876aebaba
Victor <41268339+vevt@users.noreply.github.com>
2022-05-16 14:31:25 +0300

    Initial commit

---
+++ GitHubActions/GitHubActionsTests/GitHubActionsTests.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubActionsTests.swift
+//  GitHubActionsTests
+//
+//  Created by Victor on 16.05.2022.
+//
+
+import XCTest
+@testable import GitHubActions
+
+class GitHubActionsTests: XCTestCase {
+
+    func testExample() {
+        // arrange & act
+        let model = "String"
+        
+        // assert
+        XCTAssertTrue(!model.isEmpty)
+    }
+}
