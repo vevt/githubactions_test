c1ee5bd1a0c06420f08216f6975c7fbea1eee12d
Victor <41268339+vevt@users.noreply.github.com>
2022-05-18 18:46:22 +0300

    update structure

---
+++ GitHubActions/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitHubActions
+//
+//  Created by Victor on 16.05.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
+
